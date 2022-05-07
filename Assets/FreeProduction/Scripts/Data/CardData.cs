fc502560634971576fe849259e03511517f49014
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-07 14:08:54 +0900

    カード生成処理wip

--- Assets/FreeProduction/Scripts/Data/CardData.cs
+++ Assets/FreeProduction/Scripts/Data/CardData.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
 
-[System.Serializable]
-public struct CardData
+namespace BlackJack.Data
 {
-    public int Num => 
-        _rank == RankType.A1 ? 1 :
-        _rank == RankType.A11 ? 11 :
-        _rank == RankType.J ? 10 :
-        _rank == RankType.Q ? 10 :
-        _rank == RankType.K ? 10 :
-        _num;
-
-    public RankType Rank => _rank;
-
-    public SuitType Suit => _suit;
-
-    [SerializeField]
-    [Header("計算に使うトランプの数字")]
-    private int _num;
-    
-    [SerializeField]
-    [Header("トランプの分類 絵柄, 数字")]
-    private RankType _rank;
-
-    [SerializeField]
-    [Header("トランプのスート(マーク)")]
-    private SuitType _suit;
-
-    public CardData(int num, RankType rank, SuitType suit)
+    [System.Serializable]
+    public struct CardData
     {
-        _num = num;
-        _rank = rank;
-        _suit = suit;
-    }
+        public int Num =>
+            _rank == RankType.A1 ? 1 :
+            _rank == RankType.A11 ? 11 :
+            _rank == RankType.J ? 10 :
+            _rank == RankType.Q ? 10 :
+            _rank == RankType.K ? 10 :
+            _num;
 
-    public enum RankType
-    {
-        DefaultNum = 0,
-         
-        A1,
-        A11,
-        J = 10,
-        Q = 10,
-        K = 10
-    }
+        public RankType Rank => _rank;
 
-    public enum SuitType
-    {
-        Club,
-        Diamond,
-        Heart,
-        Spade
+        public SuitType Suit => _suit;
+
+        [SerializeField]
+        [Header("計算に使うトランプの数字")]
+        private int _num;
+
+        [SerializeField]
+        [Header("トランプの分類 絵柄, 数字")]
+        private RankType _rank;
+
+        [SerializeField]
+        [Header("トランプのスート(マーク)")]
+        private SuitType _suit;
+
+        public CardData(int num, RankType rank, SuitType suit)
+        {
+            _num = num;
+            _rank = rank;
+            _suit = suit;
+        }
+
+        public enum RankType
+        {
+            DefaultNum = 0,
+
+            A1,
+            A11,
+            J = 10,
+            Q = 10,
+            K = 10
+        }
+
+        public enum SuitType
+        {
+            Club,
+            Diamond,
+            Heart,
+            Spade
+        }
     }
 }
--- Assets/FreeProduction/Scripts/Manager/CardManager.cs
+++ Assets/FreeProduction/Scripts/Manager/CardManager.cs
@@ -1,9 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using BlackJack.Data;
 
-public class CardManager : SingletonMonoBehaviour<CardManager>
+namespace BlackJack.Manager
 {
-    [SerializeField]
-    private int _deckNum = 3;
+    public class CardManager : SingletonMonoBehaviour<CardManager>
+    {
+        [SerializeField]
+        [Header("トランプのデッキをいくつ用意するか")]
+        private int _deckNum = 3;
+
+        private Card[,] _cards;
+
+        private Sprite[] _cardSprites = new Sprite[CARD_NUM];
+
+        private const int CARD_NUM = 52;
+
+        private void LoadSprite()
+        {
+            _cardSprites = Resources.LoadAll<Sprite>("Images/Cards");
+        }
+
+        private void CreateCards()
+        {
+            _cards = new Card[_deckNum, CARD_NUM];
+
+            for(int deckIndex = 0; deckIndex < _deckNum; deckIndex++)
+            {
+                for(int cardIndex = 0; cardIndex < CARD_NUM; cardIndex++)
+                {
+                    CardData.RankType rankType = GetRank(cardIndex);
+                }
+            }
+        }
+
+        private CardData.RankType GetRank(int cardIndex)
+        {
+            switch (cardIndex)
+            {
+                case  0: return CardData.RankType.A11;
+
+                case 10: return CardData.RankType.J;
+
+                case 11: return CardData.RankType.Q;
+
+                case 12: return CardData.RankType.K;
+
+                default: return CardData.RankType.DefaultNum;
+            }
+        }
+        
+        //private CardData.SuitType GetSuit(int cardIndex)
+        //{
+        //}
+    }
 }
