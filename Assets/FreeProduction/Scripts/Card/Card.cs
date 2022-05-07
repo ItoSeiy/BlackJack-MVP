820a46fc8e4012295511737e4fd5f59595270f89
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-07 13:28:02 +0900

    トランプデータ処理wip

---
+++ Assets/FreeProduction/Scripts/Card/Card.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public CardData Data => _data;
+
+    public Image Image => _image;
+
+    private CardData _data;
+
+    private Image _image;
+
+    /// <summary>
+    /// カードのデータと画像をセットアップする関数
+    /// 
+    /// メソッドをチェーン出来るようにしている
+    /// </summary>
+    public Card SetUp(CardData data, Image image, Sprite sprite)
+    {
+        _data = data;
+        _image = image;
+        _image.sprite = sprite;
+        return this;
+    }
+
+    public Card Show()
+    {
+        print($"スートは{_data.Suit} 絵柄は{_data.Rank}" +
+              $"\n画像は{_image.name} 数字は{_data.Num}");
+        return this;
+    }
+}
---
+++ Assets/FreeProduction/Scripts/Manager/CardManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : SingletonMonoBehaviour<CardManager>
+{
+    [SerializeField]
+    private int _deckNum = 3;
+}
