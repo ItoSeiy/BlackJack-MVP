3497cd1b3f779577024f0ae4eafe902c5a5ce280
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-17 18:29:35 +0900

    Input View完成

--- Assets/FreeProduction/Scripts/View/InputView.cs
+++ Assets/FreeProduction/Scripts/View/InputView.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UniRx;
 using System;
 using System.Threading.Tasks;
+using DG.Tweening;
 
 namespace BlackJack.View
 {
@@ -22,6 +23,16 @@ namespace BlackJack.View
         /// </summary>
         public IObservable<int> ObservableGameStart => _onStartButton;
 
+        /// <summary>
+        /// Hitのイベントを発行する
+        /// </summary>
+        public IObservable<string> ObservableHitButton => _onHitButton;
+
+        /// <summary>
+        /// Stayのイベントを発行する
+        /// </summary>
+        public IObservable<string> ObservableStayButton => _onStayButton;
+
         #endregion
 
         #region Inspector Variables
@@ -31,11 +42,25 @@ namespace BlackJack.View
         private int _timeToSelectable = 3000;
 
         [SerializeField]
+        [Header("ベッティングの金額の入力")]
+        private InputField _betInput;
+
+        [SerializeField]
         private Button _startButton;
 
         [SerializeField]
-        [Header("ベッティングの金額の入力")]
-        private InputField _betInput;
+        [Header("Hit, StayするボタンをまとめたCanvasGroup")]
+        private CanvasGroup _actionCanvasGroup;
+
+        [SerializeField]
+        [Header("キャンバスグループをフェードする時間")]
+        private float _fadeDuration = 0.3f;
+
+        [SerializeField]
+        private Button _hitButton;
+
+        [SerializeField]
+        private Button _stayButton;
 
         #endregion
 
@@ -56,6 +81,10 @@ namespace BlackJack.View
         /// </summary>
         private Subject<int> _onStartButton = new Subject<int>();
 
+        private Subject<string> _onHitButton = new Subject<string>();
+
+        private Subject<string> _onStayButton = new Subject<string>();
+
         #endregion
 
         #region Unity Methods
@@ -75,10 +104,34 @@ namespace BlackJack.View
         /// <summary>ゲームの進行がリセットされた際に呼び出される</summary>
         public async void Init()
         {
+            SetActionButton(false);
             await Task.Delay(_timeToSelectable);
             // 初期化されたら入力可能にする
             _betInput.interactable = true;
             _startButton.interactable = true;
+        }
+
+        /// <summary>
+        /// ボタンの表示,非表示を切りかえる
+        /// </summary>
+        /// <param name="active">
+        /// ボタンを表示するかどうか
+        /// true -> 表示
+        /// </param>
+        public void SetActionButton(bool active)
+        {
+            if(active == true)
+            {
+                _actionCanvasGroup.interactable = true;
+                _actionCanvasGroup.blocksRaycasts = true;
+                _actionCanvasGroup.DOFade(1, _fadeDuration);
+            }
+            else
+            {
+                _actionCanvasGroup.interactable= false;
+                _actionCanvasGroup.blocksRaycasts = false;
+                _actionCanvasGroup.DOFade(0, _fadeDuration);
+            }
         }
 
         #endregion
@@ -96,6 +149,10 @@ namespace BlackJack.View
             });
 
             _startButton.onClick.AddListener(OnStartButton);
+
+            _hitButton.onClick.AddListener(OnHitButton);
+
+            _stayButton.onClick.AddListener(OnStayButton);
         }
 
         private void OnStartButton()
@@ -112,6 +169,16 @@ namespace BlackJack.View
         private void SetBetValue(int value)
         {
             _betValue = value;
+        }
+
+        private void OnHitButton()
+        {
+            _onHitButton.OnNext("Hit");
+        }
+
+        private void OnStayButton()
+        {
+            _onStayButton.OnNext("Stay");
         }
 
         #endregion
