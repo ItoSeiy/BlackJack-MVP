ec025ace009381cbbec556fbee5d38c50768988d
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-17 14:59:28 +0900

    入力に関するPresenter

---
+++ Assets/FreeProduction/Scripts/Presenter/InputPresenter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackJack.Presenter
+{
+    public class InputPresenter : MonoBehaviour
+    {
+        #region Properties
+        #endregion
+
+        #region Inspector Variables
+        #endregion
+
+        #region Member Variables
+        #endregion
+
+        #region Constant
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        #endregion
+
+        #region Enums
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+
+        private void SubscribeInput()
+        {
+
+        }
+
+        #endregion
+    }
+}
