e0e70c7656167470ba05b8db3d2370188667f4bd
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-18 23:39:10 +0900

    クレジットに関するプレゼンター

---
+++ Assets/FreeProduction/Scripts/Presenter/CreditPresenter.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BlackJack.View;
+using BlackJack.Model;
+using UniRx;
+using BlackJack.Manager;
+using BlackJack.Data;
+
+namespace BlackJack.Presenter
+{
+    /// <summary>
+    /// クレジット全般のPresenter
+    /// </summary>
+    public class CreditPresenter : MonoBehaviour
+    {
+        #region Inspector Variables
+
+        [SerializeField]
+        private CreditView _creditView;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            Subscribe();
+            SetUp();
+            BoardModel.Instance.OnInitialize += SetUp;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Subscribe()
+        {
+            BetModel.Instance.ObservableReturnBetValue.Subscribe(x =>
+            { 
+                _creditView.SetWinBetText(x);
+
+                CreditDataManager.Instance.UpdateCreditData
+                    (new CreditData(CreditDataManager.Instance.CreditData.Credit + x));
+            });
+
+            BetModel.Instance.ObservableSetBetValue.Subscribe(x => _creditView.SetBetValue(x));
+        }
+
+        private void SetUp()
+        {
+            _creditView.SetCreditText(CreditDataManager.Instance.CreditData.Credit);
+            _creditView.Init();
+        }
+
+        #endregion
+    }
+}
